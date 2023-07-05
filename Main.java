9fbc6d4f9fd9ccf66eebc40637e2b63908908a53
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-05 23:06:11 +0530

    Added: Ability to create Linked List by adding 30 and 56 to 70

--- Main.java
+++ Main.java
@@ -5,10 +5,11 @@ public class Main {
         System.out.println("Welcome to the LinkedList");
         System.out.println();
         LinkedList ls = new LinkedList();
-        ls.addFirstNode(56);
-        ls.addFirstNode(30);
+        // Adding nodes in reverse order to get the desired sequence
         ls.addFirstNode(70);
-
+        ls.addFirstNode(30);
+        ls.addFirstNode(56);
+        System.out.println("LinkedList Sequence: ");
         ls.printNode();
 
     }
